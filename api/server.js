9e1302316927897395806a9a4eed504741f08dc3
Hargobind Atwal <ethaipath@gmail.com>
2019-03-18 13:55:35 -0700

    added morgan dep

--- api/server.js
+++ api/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan')
 
 const authRouter = require('../auth/auth-routes.js')
 
 const server = express();
 
-// test endpoint
-server.get('/', (req, res) => {
-  res.status(200).json({'message': 'server up!'})
-})
-
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(morgan('dev'))
+
+// test endpoint
+server.get('/', (req, res) => {
+  res.status(200).json({message: 'server up!'})
+})
 
 server.use('/api', authRouter);
 
