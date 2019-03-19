f089f0ba4ebacd769988af0acf8beb4766ec79f1
bluelogix <bluelogix010@gmail.com>
2019-03-19 15:40:39 -0400

    adding knex migration to make article part

--- data/migrations/20190318200237_articlesTable.js
+++ data/migrations/20190318200237_articlesTable.js
@@ -1,16 +1,34 @@
 
 exports.up = function(knex, Promise) {
-  
+ return knex.schema.createTable('articles', articles => {
+    articles.increments();
+    //both title and content are needed in order to fill out form
+    articles
+        .string('title', 128)
+        .notNullable()
+    articles
+       .text('content', 255)
+       .notNullable();
+    //relationship to the board id
+    articles
+      .integer("board_id")
+      .unsigned()
+      .references("id")
+      .inTable("boards");
+    //optional additions
+    articles
+        .string('abstract', 255); //for extra details on 
+    articles
+        .text('image', 128); //adding image placement for react portion 
+    articles
+        .text('category', 255); //for filtering purposes on react portion
+
+      });
 };
+
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('articles');
 };
 
 
-// needed for article page: 
-//Title:,
-//Content: url,
-//Abstract: 
-//Category:,
-//Image: ?? (maybe)
